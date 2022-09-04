758ae303f7568d14fd4d34763642905b2e2b0702
Vladimir <vladimir.yakimov.9811@gmail.com>
2022-09-04 22:27:30 +0300

    Training 'Grasshopper - Debug' kata.

---
+++ Grasshopper_Debug.js
@@ -0,0 +1,9 @@
+function weatherInfo (temp) {
+    let c = convertToCelsius(temp)
+    if (c > 0) return (c + " is above freezing temperature")
+    else return (c + " is freezing temperature")
+}
+
+function convertToCelsius (temperature) {
+    return (temperature - 32) * (5 / 9)
+}
