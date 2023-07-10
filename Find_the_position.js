79cf6ea27ef06918498127944dfac659cc917714
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-07-10 21:35:50 +0300

    Training 'Find the position!' kata.

---
+++ Find_the_position.js
@@ -0,0 +1,4 @@
+function position(letter){
+    let arr_en = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    return `Position of alphabet: ${arr_en.indexOf(letter) + 1}`
+}
